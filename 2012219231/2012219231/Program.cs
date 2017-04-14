f86fd02c1a2828f1c0727111dd26e09fa1e4c345
Ugo Soto N <Ugo Soto N>
2017-04-14 18:56:29 -0500

    commit de prueba

---
+++ 2012219231/2012219231/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2012219231
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mensaje de Prueba");
+        }
+    }
+}
